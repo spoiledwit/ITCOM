b94c5c90453bd358b066522cf528a8f98ae16df1
spoiledwit <spoiledwit@gmail.com>
2022-09-25 14:43:55 +0500

    Added PrimaryEventsData.js File

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import PrimaryEvents from './PrimaryEventsData';
 
 function App() {
   return (
---
+++ src/PrimaryEventsData.js
@@ -0,0 +1,57 @@
+import delegate from "./images/delegate.webp"
+import mocktrialscover from "./images/mocktrialscover.webp"
+import ylpcover from "./images/ylpcover.webp"
+import sspcover from "./images/sspcover.webp"
+import pscover from "./images/pscover.webp"
+
+const PrimaryEvents = [ 
+    {
+        id:1,
+        title: "ITUMUN Conference",
+        info: <>ITUMUN conference is the ITU’s prestigious Model United Nations conference held annually on Campus.
+        It is a four-day international relations simulation for the college students where besides getting engaged into heated debates over various topics and representing different countries, delegates, by assuming the roles of leaders, learn about diplomacy and get a platform to debate the most pressing issues of the world.
+        Throughout the conference, delegates learn various skills, including public speaking, negotiations, leadership and team building.
+        Besides the debates, delegates get entertained through mind-blowing socials throughout the four-day conference and get the chance to expand their network, build relationships, and understand different perspectives and engage other cultures.</>,
+        image: delegate,
+    },
+    {
+        id: 2,
+        title: "Mock Trials",
+        info: <>Mock Trials is another flagship event by ITUMUN in which students simulate a real trial. It exposes students to legal topics and provide them with an opppurtunity to develop advocacy, critical thinking, and develp relationships with justice sector professionals.
+        Students throught out the trials learn numerous skills and meet amazing people of legal field.
+        <br />
+        It involves a Judge (could be a visitor to class with legal experience),  A prosecutor(s) or plaintiff's attorney(s) in a civil case defense attorney(s), the witnesses for the prosecution, the witnesses for the defense, and bailiff (swears in witnesses and marks evidence).
+        </>, 
+        image: mocktrialscover,
+    },
+    {
+        id: 3,
+        title: "ITUMUN Social Service Programs",
+        info: <>
+        ITUMUN's Social Service program is a set of volunteer works hosted by ITUMUN throughout the year to promote positive and sustainable growth within the communities. Students all over the country get to participate, improving social connections, improving mental and physical health, and having high employability. Participants travel throughout the country and get engaged in various volunteer works hosted by ITUMUN, covering but not limited to 
+<b> Environmental, </b>
+<b>Animals, </b>
+<b>Social, </b>
+<b>Healthcare, </b>
+<b> and Sports and leisure events.</b>
+        </>,
+        image: sspcover,
+    },
+    {
+        id: 4,
+        title: "Youth Leadership Programs",
+        info: <>
+        Youth Leadership Program is a set of leadership training where students get engaged in various problem-solving and administrative tasks to help them build the leadership and social skills that are necessary for personal growth. These training includes project planning and delegating, conflict resolution, problem solving, time management, and leading under pressure tasks. Leadership program happen throughout the year on campus.</>, 
+        image: ylpcover,
+    },
+    {
+        id: 5,
+        title: "Public Speaking Fests",
+        info: <>
+        ITUMUN organizes a wide range of Public Speaking Events throughout the year both on campus and virtually. It includes seminars, and conferences along with socials, virutal events, award ceremonies, and many more. Through these programs, ITUMUN aims to help the students boost their confidence and drive change in society through good public speaking skills.
+        </>,
+        image: pscover,
+    }
+];
+
+export default PrimaryEvents;
